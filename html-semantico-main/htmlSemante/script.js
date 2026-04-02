4c72140a7f510ecffea2887d6bce1900ffdae063
guilhermepereirafelipe-svg <guilherme.pereira.felipe@escola.pr.gov.br>
2026-04-02 16:29:50 -0300

    Add files via upload

---
+++ html-semantico-main/htmlSemante/script.js
@@ -0,0 +1,8 @@
+function alterarNome() {
+let trocarNome = document.getElementById('inputNome').value;
+if (trocarNome === '') { 
+alert ('digite um nome!')
+} else {
+    document.getElementbyId('nomeUsuario').innerHTML=trocarNome;
+}
+}
